84554b57bae69fa4e5a0d2c8f1ee9a047b58d735
Roberto J <robertonsj31@gmail.com>
2023-11-01 11:30:51 -0300

    Geometric mean algorithm

--- src/exercicios/ruhanconceicao/MediaGeometrica.java
+++ src/exercicios/ruhanconceicao/MediaGeometrica.java
@@ -4,11 +4,35 @@
  * and open the template in the editor.
  */
 package exercicios.ruhanconceicao;
+import java.text.DecimalFormat;
+import java.util.Scanner;
 
 /**
  *
  * @author logistica
  */
 public class MediaGeometrica {
+    /*Write a algorithm to calculate the geometric mean of three numbers
+    given by user.
+    mean = square root of de a * b * c, index 3 
+    */
+    public static void main(String args[]){
+    Scanner scanner = new Scanner(System.in);
+    DecimalFormat format = new DecimalFormat(".######");
     
+    System.out.print("Enter the first number: ");
+    double firstNumber = scanner.nextInt();
+    System.out.print("Enter the second number: ");
+    double secondNumber = scanner.nextInt();
+    System.out.print("Enter the third number: ");
+    double thirdNumber = scanner.nextInt();
+    
+    double geometricMean = 
+            Math.pow(firstNumber * secondNumber *  thirdNumber, 1.0/3.0);
+    
+    System.out.println("The geometric average is: " + format.format(geometricMean));
+    scanner.close();
+    
+    }
+   
 }
