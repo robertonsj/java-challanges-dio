b50995d84cdb24c7237c8f7ac0097dbed5e20287
Roberto J <robertonsj31@gmail.com>
2023-11-09 12:57:33 -0300

    Plano Cartesiano inicio

---
+++ src/exercicios/ruhanconceicao/PlanoCartesiano.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+Calculate the distance between two points in a cartesian plane.*/
+package exercicios.ruhanconceicao;
+
+/**
+ *
+ * @author logistica
+ */
+public class PlanoCartesiano {
+    private double distanciaEntreDoisPontos;
+    private double coordenadaPonto1;
+    private double coordenadaPonto2;
+    PlanoCartesiano(double[] pontos){
+        
+    }
+   
+    
+            
+    
+}
