f584418ec78f62aa295de8b2ba65b28249ff84c0
Roberto J <robertonsj31@gmail.com>
2023-12-07 11:07:26 -0300

    Task Cartesian Plane done

--- src/exercicios/ruhanconceicao/PlanoCartesiano.java
+++ src/exercicios/ruhanconceicao/PlanoCartesiano.java
@@ -5,22 +5,37 @@
  */
 
 /* 
-Calculate the distance between two points in a cartesian plane.*/
+Calculate the distance between points in the cartesian plane.*/
 package exercicios.ruhanconceicao;
+
+import java.util.Scanner;
 
 /**
  *
  * @author logistica
  */
 public class PlanoCartesiano {
-    private double distanciaEntreDoisPontos;
-    private double coordenadaPonto1;
-    private double coordenadaPonto2;
-    PlanoCartesiano(double[] pontos){
-        
+    private double distanceBetweenPoints,
+            coordenatePoints[] = new double[4]; 
+    
+
+    public PlanoCartesiano(double x1, double y1, double x2, double y2){
+        coordenatePoints[0] = x1;
+        coordenatePoints[1] = y1;
+        coordenatePoints[2] = x2;
+        coordenatePoints[3] = y2;
+        this.distanceBetweenPoints = getDistance(coordenatePoints);
     }
-   
     
-            
+    public double getDistance(double points[]){
+        double distance;
+        distance = Math.sqrt(Math.pow(points[2] - points[0], 2) + 
+                Math.pow(points[3] - points[1], 2));
+        return distance;
+    }
     
+    public double getDistanceBetweenPoints() {
+        return distanceBetweenPoints;
+    }
+
 }
--- src/exercicios/ruhanconceicao/Principal.java
+++ src/exercicios/ruhanconceicao/Principal.java
@@ -5,28 +5,24 @@ import java.text.DecimalFormat;
 
 public class Principal {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner scan = new Scanner(System.in);
-                DecimalFormat format = new DecimalFormat(".####");
-		
-		System.out.print("First: ");
-		double first = scan.nextDouble();
-		System.out.print("Second: ");
-		double second = scan.nextDouble();
-		System.out.print("Third: ");
-		double third = scan.nextDouble();
-		
-                /*
-                Kinetic Energy.
-		EnergiaCinetica energiaC = new EnergiaCinetica(massa, velocidade);
-                PerimetroCirculo  perimetroC = new PerimetroCirculo(radius);
-                MediaGeometrica mGeometrica = new MediaGeometrica(first, second, third);
-		System.out.println("The geometric mean is approximately: " + 
-                        format.format(mGeometrica.geometricMean));
-                        */
-                
-	}
-	
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        Scanner scan = new Scanner(System.in);
+        DecimalFormat format = new DecimalFormat(".##");
+
+        
+//        PlanoCartesiano planoCartesiano = new PlanoCartesiano(3, 4, 7, 1);
+//        double distance = planoCartesiano.getDistanceBetweenPoints();
+//        System.out.println("\nDistance entre dois pontos: " + format.format(distance));
+
+        /*
+         Kinetic Energy.
+         EnergiaCinetica energiaC = new EnergiaCinetica(massa, velocidade);
+         PerimetroCirculo  perimetroC = new PerimetroCirculo(radius);
+         MediaGeometrica mGeometrica = new MediaGeometrica(first, second, third);
+         System.out.println("The geometric mean is approximately: " + 
+         format.format(mGeometrica.geometricMean));
+         */
+    }
 
 }
